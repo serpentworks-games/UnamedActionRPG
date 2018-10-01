e4cb42a3edf3a2b90c43886073f1130eafc5c2a7
Alexis Barta <alexisbarta@outlook.com>
2018-10-01 00:38:25 -0700

    New sprites!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,19 +11,21 @@ public class PlayerMovement : MonoBehaviour {
     float h;
     float v;
     bool facingRight = true;
+    Animator anim;
 
-    public GameObject meleeSocket;
+   // public GameObject meleeSocket;
     public GameObject rangedSocket;
     bool weaponSwitch;
 
     private void Awake()
     {
         pRB = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Use this for initialization
     void Start () {
-        weaponSwitch = true;
+        //weaponSwitch = true;
 	}
 	
 	// Update is called once per frame
@@ -31,14 +33,16 @@ public class PlayerMovement : MonoBehaviour {
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
 
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            weaponSwitch = !weaponSwitch;
-        }
-        meleeSocket.SetActive(weaponSwitch);
-        rangedSocket.SetActive(!weaponSwitch);
+        //if (Input.GetKeyDown(KeyCode.Q))
+       // {
+       //     weaponSwitch = !weaponSwitch;
+       // }
+      //  meleeSocket.SetActive(weaponSwitch);
+      //  rangedSocket.SetActive(!weaponSwitch);
         moveInput = new Vector2(h, v);
         moveVelocity = moveInput.normalized * moveSpeed;
+        
+        anim.SetFloat("speed", moveInput.magnitude);
     }
 
     private void FixedUpdate()

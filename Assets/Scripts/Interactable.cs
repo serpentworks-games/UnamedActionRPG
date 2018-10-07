d08bb4b365b9ad25d9f5a4366f187be46c3a9d03
Alexis Barta <alexisbarta@outlook.com>
2018-10-06 21:38:59 -0700

    NPC Icon stuff
    Bc that's def what I should be focusing on rn

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+
+    public float radius = 3f;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
---
+++ Assets/Scripts/InvSlot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvSlot : MonoBehaviour {
+
+    Inventory inv;
+    public int i;
+
+	// Use this for initialization
+	void Start () {
+        inv = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.childCount <= 0)
+        {
+            inv.isFull[i] = false;
+        }
+	}
+    public void DiscardItem()
+    {
+        foreach (Transform item in transform)
+        {
+            item.GetComponent<SpawnDroppedItem>().SpawnItem();
+            GameObject.Destroy(item.gameObject);
+        }
+    }
+}
